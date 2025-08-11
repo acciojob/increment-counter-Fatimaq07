645b391d5880c1af9a3fcc16a82ea4b2d1b9cffa
FATIMA QURESHI <115686917+Fatimaq07@users.noreply.github.com>
2025-08-12 00:09:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+//your JS code here. If requirlet counter = document.getElementById("counter");
+    let incrementBtn = document.getElementById("incrementBtn");
+    incrementBtn.addEventListener("click", function() {
+      alert(counter.textContent);
+      counter.textContent = parseInt(counter.textContent) + 1;
+    });
